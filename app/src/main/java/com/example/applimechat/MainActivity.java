772516a0b0cdfca7f278814dff3be12a960d0ce0
Narrent <maninkenrz@gmail.com>
2024-06-19 14:06:45 +0300

    NL-8, NL-9 Додано виделення клієнтскіх текстів

--- app/src/main/java/com/example/applimechat/MainActivity.java
+++ app/src/main/java/com/example/applimechat/MainActivity.java
@@ -108,7 +108,6 @@ public class MainActivity extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError error) {}
         });
 
-        // Відстеження кількості користувачів
         userCountRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
